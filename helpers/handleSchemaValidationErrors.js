92263980424f866ef5024f76e0f1dea38384bb4a
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-09-25 22:34:55 +0300

    mongo db error number was corrected

--- helpers/handleSchemaValidationErrors.js
+++ helpers/handleSchemaValidationErrors.js
@@ -1,5 +1,5 @@
 const hasConflict = ({ name, code }) =>
-  name === 'MongoServerError' && code === 1100
+  name === 'MongoServerError' && code === 11000
 
 const handleSchemaValidationErrors = (error, _, next) => {
   error.status = hasConflict(error) ? 409 : 400
