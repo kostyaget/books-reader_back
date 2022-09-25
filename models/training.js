a41ccfd1b7e4c646aa037b60d24a0758fa717983
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-09-25 14:44:51 +0300

    training model implementation was started

---
+++ models/training.js
@@ -0,0 +1,13 @@
+const { Schema, model } = require('mongoose')
+
+const { handleSchemaValidationErrors } = require('../helpers')
+
+const trainingSchema = new Schema({}, { versionKey: false, timestamps: true })
+
+trainingSchema.post('save', handleSchemaValidationErrors)
+
+const Training = model('book', trainingSchema)
+
+module.exports = {
+  Book: Training,
+}
