9054b712c7f22f06f31544ff2a68495225ce0efa
Spec27 <shkvara27@gmail.com>
2022-09-26 20:44:12 +0300

    ok

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const controller = require('../../controllers/books')
-/* const { validationBody } = require('../../middlewares/validationBody') */
-const { controllerWrapper } = require('../../helpers')
+
+const { ctrlWrapper } = require('../../helpers')
 const router = express.Router()
 
-router.post('/', controllerWrapper(controller.add))
+router.post('/', ctrlWrapper(controller.add))
 
 module.exports = router
