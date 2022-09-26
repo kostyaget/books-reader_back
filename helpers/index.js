c2c739dc1851f7a706531d59c0c996f6dfe4774f
Spec27 <shkvara27@gmail.com>
2022-09-26 16:27:30 +0300

    Endpoin books add finished

--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,11 @@
 const RequestError = require('./RequestError')
 const handleSchemaValidationErrors = require('./handleSchemaValidationErrors')
+const controllerWrapper = require('./controllerWrapper')
 const patterns = require('./patterns')
 
 module.exports = {
   handleSchemaValidationErrors,
   patterns,
   RequestError,
+  controllerWrapper,
 }
