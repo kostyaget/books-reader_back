05bea20b60f2e5428499cd8b9e95051aeb6df95d
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-09-27 22:15:08 +0300

    prettier code styles were chanded

--- app.js
+++ app.js
@@ -6,10 +6,9 @@ require('dotenv').config()
 const booksRouter = require('./routes/api/books')
 const app = express()
 
-const authRouter = require("./routes/api/auth");
+const authRouter = require('./routes/api/auth')
 
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
-
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger))
 app.use(cors())
@@ -18,7 +17,7 @@ app.use(express.static('public'))
 
 app.use('/api/books', booksRouter)
 
-app.use("/api/auth", authRouter);
+app.use('/api/auth', authRouter)
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
