de69407d3917b1e035ecbc84db265f1682eafe86
Spec27 <shkvara27@gmail.com>
2022-09-27 22:57:15 +0300

    fixed

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,13 @@
 const express = require('express')
 
-const { controllerWrapper } = require('../../helpers')
+const { ctrlWrapper } = require('../../helpers')
 const { auth } = require('../../middlewares')
 const { logout, googleAuth, googleRedirect } = require('../../controllers/auth')
 
 const router = express.Router()
 
-router.post('/logout', auth, controllerWrapper(logout))
-router.get('/google', controllerWrapper(googleAuth))
-router.get('/google-redirect', controllerWrapper(googleRedirect))
+router.post('/logout', auth, ctrlWrapper(logout))
+router.get('/google', ctrlWrapper(googleAuth))
+router.get('/google-redirect', ctrlWrapper(googleRedirect))
 
 module.exports = router
