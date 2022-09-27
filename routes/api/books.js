4c14b738aeeaddea9fd1fa9ff7cbb215222af15a
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-09-27 14:35:17 +0300

    validate body was linked

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const controller = require('../../controllers/books')
 const { updateResume } = require('../../controllers/books')
-
+const { validateBody } = require('../../middlewares')
 const { ctrlWrapper } = require('../../helpers')
+const { updateResumeSchema } = require('../../models')
+
 const router = express.Router()
 
 router.post('/', ctrlWrapper(controller.add))
-router.patch('/:bookId/resume', ctrlWrapper(updateResume))
+router.patch(
+  '/:bookId/resume',
+  validateBody(updateResumeSchema),
+  ctrlWrapper(updateResume)
+)
 
 module.exports = router
