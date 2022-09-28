f3a1779c126437448f0bcdfa193071be81578856
Serhii Korchemkin <skorchemkin1987@gmail.com>
2022-09-28 10:12:22 +0300

    fixed

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const booksRouter = require('./routes/api/books')
 const app = express()
 
 const authRouter = require('./routes/api/auth')
-// const authRouter = require('./routes/api/users')
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,9 +1,12 @@
 
 const register = require('./register');
 const login = require('./login');
+const { googleAuth, googleRedirect } = require("./googleLogin");
 
 module.exports = {
     register,
     login,
+    googleAuth,
+    googleRedirect,
 }
 
