31199cdd16e0297848bfab842c6cc146190164ff
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-09-28 12:55:10 +0300

    add user results was implemented

--- controllers/users/addResults.js
+++ controllers/users/addResults.js
@@ -1,15 +1,26 @@
 const { RequestError } = require('../../helpers')
+const { User } = require('../../models')
 
 const addResults = async (req, res) => {
   const { id: userId } = req.params
   const { progress, _id: authenticatedUserId } = req.user
-  console.log(userId, authenticatedUserId)
 
   if (userId !== authenticatedUserId.toString()) throw RequestError(403)
 
   progress.push(req.body)
 
-  res.json({ ...req.user })
+  const updatedUserResults = await User.findByIdAndUpdate(
+    userId,
+    { progress },
+    {
+      new: true,
+      select: {
+        progress: 1,
+      },
+    }
+  )
+
+  res.json(updatedUserResults)
 }
 
 module.exports = { addResults }
