87e97533f02f01de4ed20322e8b94551faaaa04a
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-09-28 12:27:39 +0300

    user rights validation added

--- controllers/users/addResults.js
+++ controllers/users/addResults.js
@@ -1,6 +1,15 @@
+const { RequestError } = require('../../helpers')
+
 const addResults = async (req, res) => {
-  console.log(req.user)
-  res.json({ message: 'ok' })
+  const { id: userId } = req.params
+  const { progress, _id: authenticatedUserId } = req.user
+  console.log(userId, authenticatedUserId)
+
+  if (userId !== authenticatedUserId.toString()) throw RequestError(403)
+
+  progress.push(req.body)
+
+  res.json({ ...req.user })
 }
 
 module.exports = { addResults }
