e06dc18574a7755838d04ac0d8269e208dc5e30c
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-09-28 13:37:40 +0300

    code duplication was deleted

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,20 +2,14 @@ const express = require('express')
 
 const { ctrlWrapper } = require('../../helpers')
 const {
-  auth,
   validateBody,
   authenticateUser,
   validateRequestId,
 } = require('../../middlewares')
-const { logout, googleAuth, googleRedirect } = require('../../controllers/auth')
 const { addResults } = require('../../controllers/users')
 const { addResultSchema } = require('../../models')
 
 const router = express.Router()
-
-router.post('/logout', auth, ctrlWrapper(logout))
-router.get('/google', ctrlWrapper(googleAuth))
-router.get('/google-redirect', ctrlWrapper(googleRedirect))
 
 router.patch(
   '/:id/results',
