fe664b85c66498d453eeb229b4bcf038086bfdfc
Spec27 <shkvara27@gmail.com>
2022-09-28 22:29:01 +0300

    swagger register and login endpoint

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,29 +1,29 @@
-const bcrypt = require('bcrypt');
-const { RequestError } = require('../../helpers');
+const bcrypt = require('bcrypt')
+const { RequestError } = require('../../helpers')
 
-const { User } = require('../../models/user');
+const { User } = require('../../models/user')
 
 const register = async (req, res, next) => {
-    const { username, email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (user) {
-        throw RequestError(409, "Email already registered");
-    }
-    const hashPassword = await bcrypt.hash(password, 10);
-    const result = await User.create({
-        username,
-        email,
-        password: hashPassword,
-        repeat_Password: hashPassword,
-    });
-    
-    res.status(201).json({
-        user: {
-            username:result.username,
-            email:result.email,
-        },
-        
-    });
+  const { username, email, password } = req.body
+  const user = await User.findOne({ email })
+  if (user) {
+    throw RequestError(409, 'Email already registered')
+  }
+  const hashPassword = await bcrypt.hash(password, 10)
+  const result = await User.create({
+    username,
+    email,
+    password: hashPassword,
+    repeat_Password: hashPassword,
+  })
+
+  res.status(201).json({
+    user: {
+      username: result.username,
+      email: result.email,
+      _id: result._id,
+    },
+  })
 }
 
-module.exports = register;
+module.exports = register
