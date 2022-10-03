37d5b7eca1201a9e4b89cf68d7ead25b71cbf5a8
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-10-03 13:06:47 +0300

    verification token check fixed

--- controllers/users/verificationTokenCheck.js
+++ controllers/users/verificationTokenCheck.js
@@ -2,22 +2,18 @@ const { User } = require('../../models')
 const { RequestError } = require('../../helpers')
 
 const verificationTokenCheck = async (req, res, next) => {
-  try {
-    const { token: verificationToken } = req.params
-    const user = await User.findOne({ verificationToken })
-    if (!user) throw RequestError(404, 'User not Found')
+  const { token: verificationToken } = req.params
+  const user = await User.findOne({ verificationToken })
+  if (!user) throw RequestError(404, 'User not Found')
 
-    await User.findByIdAndUpdate(user._id, {
-      verified: true,
-      verificationToken: '',
-    })
+  await User.findByIdAndUpdate(user._id, {
+    verified: true,
+    verificationToken: '',
+  })
 
-    res.status(200).json({
-      message: 'Verification successful',
-    })
-  } catch (error) {
-    next(error)
-  }
+  res.status(200).json({
+    message: 'Verification successful',
+  })
 }
 
 module.exports = { verificationTokenCheck }
