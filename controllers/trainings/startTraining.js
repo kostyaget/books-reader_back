9179b2ad8185aca691ec37ed2b54ea5d59c48fde
Dmytro Gashutin <dm.gashutin@gmail.com>
2022-10-04 18:30:59 +0300

    change startTraining logic

--- controllers/trainings/startTraining.js
+++ controllers/trainings/startTraining.js
@@ -1,16 +1,32 @@
 const { Training, Book } = require('../../models')
 
-const startTraining = async (req, res) => {
-  const { _id: participator } = req.user
-  const { id: bookId } = req.params
-  const { startDate, finishDate, book, pageAmount } = req.body
-  const body = { startDate, finishDate, book, pageAmount }
 
-  const findBook = await Book.findOne(bookId)
-  if (!findBook) {
-    throw new Error('Book is not found')
+const startTraining = async (req, res) => {
+  const { _id } = req.user;
+  const newTraining = { ...req.body, participator: req.user._id };
+  const findUserTraining = await Training.findOne({ participator: _id });
+  if (findUserTraining) {
+    const { id } = findUserTraining;
+    const { startDate, finishDate, book } = newTraining;
+    const result = await Training.findByIdAndUpdate(
+      id,
+      { startDate, finishDate, book },
+      { new: true }
+    );
+     await Book.updateMany(
+      { _id: { $in: result.book } },
+     {status:"ACTIVE"},
+      { multi: true }
+    );
+    res.status(201).json(result)
+  } else {
+    const result = await Training.create(newTraining);
+    await Book.updateMany(
+      { _id: { $in: result.books} },
+  {status: "ACTIVE"},
+      { multi: true }
+    );
+      res.status(201).json(result)
   }
-  const result = await Training.create({ ...body, participator, pageAmount })
-  res.status(201).json(result)
 }
 module.exports = startTraining
